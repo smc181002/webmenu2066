845869d03b5a04c8de90478c7db4309147d43384
smc181002 <meherchaitanya18802@gmail.com>
2020-11-21 20:24:15 +0530

    :art: created specific js files for cards variables

---
+++ src/json/maincards.js
@@ -0,0 +1,34 @@
+let cards = [
+  {
+    title: "Docker Operations",
+    image: "https://img.icons8.com/color/48/000000/docker.png",
+    altImg: "Docker",
+    backText: "Containers",
+  },
+  {
+    title: "AWS Operations",
+    image: "https://img.icons8.com/color/48/000000/docker.png",
+    altImg: "AWS",
+    backText: "Cloud Comp",
+  },
+  {
+    title: "Hadoop Operations",
+    image: "https://img.icons8.com/color/48/000000/docker.png",
+    altImg: "Hadoop",
+    backText: "Map Reduce",
+  },
+  {
+    title: "LVM partitions",
+    image: "https://img.icons8.com/color/48/000000/docker.png",
+    altImg: "Hadoop",
+    backText: "Partitions",
+  },
+  {
+    title: "YUM repo config",
+    image: "https://img.icons8.com/color/48/000000/docker.png",
+    altImg: "Hadoop",
+    backText: "Repository",
+  },
+];
+
+export default cards;
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,38 +1,6 @@
 <script>
   import MainCard from "../components/MainCard.svelte";
-
-  let cards = [
-    {
-      title: "Docker Operations",
-      image: "https://img.icons8.com/color/48/000000/docker.png",
-      altImg: "Docker",
-      backText: "Containers",
-    },
-    {
-      title: "AWS Operations",
-      image: "https://img.icons8.com/color/48/000000/docker.png",
-      altImg: "AWS",
-      backText: "Cloud Comp",
-    },
-    {
-      title: "Hadoop Operations",
-      image: "https://img.icons8.com/color/48/000000/docker.png",
-      altImg: "Hadoop",
-      backText: "Map Reduce",
-    },
-    {
-      title: "LVM partitions",
-      image: "https://img.icons8.com/color/48/000000/docker.png",
-      altImg: "Hadoop",
-      backText: "Partitions",
-    },
-    {
-      title: "YUM repo config",
-      image: "https://img.icons8.com/color/48/000000/docker.png",
-      altImg: "Hadoop",
-      backText: "Repository",
-    },
-  ];
+  import cards from "../json/maincards.js";
 </script>
 
 <style>
